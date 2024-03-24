7a1debd0f077527ad00acf480ba2bba4f920670f
AmberParmaar <160378243+Amberparmaar@users.noreply.github.com>
2024-03-24 22:46:23 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 +1);
+
+const answers = await inquirer.prompt([
+    {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please Guess a number between 1 - 10:"
+    }
+])
+if(answers.userGuessedNumber === randomNumber){
+    console.log("congratulations! You guessed it right");
+}else{
+    console.log("Sorry! You guessed it wrong");
+}
+
+
